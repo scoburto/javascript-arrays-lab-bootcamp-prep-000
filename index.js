205a38c8ee2f7c54389e28b5b1c980eb69fa3470
scoburto@gmail.com <scoburto@gmail.com>
2018-03-04 18:26:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const app = "I don't do much."
+
+function destructivelyAppendKittens(list,felix) {
+  list.push(felix);
+  return list;
+}
+
+function destructivelyPrependKittens(list,felix) {
+  list.unshift(felix);
+  return list;
+}
+
+function destructivelyRemoveLastKitten(list) {
+  list.pop();
+  return list;
+}
+
+function destructivelyRemoveFirstKitten(list) {
+  list.shift();
+  return list;
+}
